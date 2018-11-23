9469b46055092e4839c4ecca4bb00edb7cf1762d
christ2 <christian.chandra88@gmail.com>
2018-11-23 12:48:42 +0700

    Create users.module.js

---
+++ store/users.module.js
@@ -0,0 +1,66 @@
+import { user } from '../_services';
+
+const state = {
+    all: {}
+};
+
+const actions = {
+    getAll({ commit }) {
+        commit('getAllRequest');
+
+        user.getAll()
+            .then(
+                users => commit('getAllSuccess', users),
+                error => commit('getAllFailure', error)
+            );
+    },
+
+    delete({ commit }, id) {
+        commit('deleteRequest', id);
+
+        user.delete(id)
+            .then(
+                user => commit('deleteSuccess', id),
+                error => commit('deleteSuccess', { id, error: error.toString() })
+            );
+    }
+};
+
+const mutations = {
+    getAllRequest(state) {
+        state.all = { loading: true };
+    },
+    getAllSuccess(state, users) {
+        state.all = { items: users };
+    },
+    getAllFailure(state, error) {
+        state.all = { error };
+    },
+    deleteRequest(state, id) {
+        state.all.items = state.all.items.map(user =>
+            user.id === id
+                ? { ...user, deleting: true }
+                : user
+        )
+    },
+    deleteSuccess(state, id) {
+        state.all.items = state.all.items.filter(user => user.id !== id)
+    },
+    deleteFailure(state, { id, error }) {
+        state.all.items = state.items.map(user => {
+            if (user.id === id) {
+                const { deleting, ...userCopy } = user;
+                return { ...userCopy, deleteError: error };
+            }
+
+            return user;
+        })
+    }
+};
+
+export const users = {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+};
