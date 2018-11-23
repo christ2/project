6d05d867e06c44000a04d439ffa28efee6b05e37
christ2 <christian.chandra88@gmail.com>
2018-11-23 13:57:05 +0700

    Update Home.vue

--- Home.vue
+++ Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Hi {{account.user.firstName}}!</h1>
+        <h3>User list:</h3>
         <em v-if="users.loading">Loading users...</em>
         <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
         <ul v-if="users.items">
@@ -8,6 +9,7 @@
                 {{user.firstName + ' ' + user.lastName}}
                 <span v-if="user.deleting"><em> - Deleting...</em></span>
                 <span v-else-if="user.deleteError" class="text-danger"> - ERROR: {{user.deleteError}}</span>
+                <span v-else> - <a @click="updateUser(user.id)" class="text-danger">Update</a></span>
                 <span v-else> - <a @click="deleteUser(user.id)" class="text-danger">Delete</a></span>
             </li>
         </ul>
@@ -33,7 +35,8 @@ export default {
     methods: {
         ...mapActions('users', {
             getAllUsers: 'getAll',
-            deleteUser: 'delete'
+            deleteUser: 'delete',
+            updateUser: 'update'
         })
     }
 };
